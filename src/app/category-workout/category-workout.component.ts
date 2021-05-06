1d6487884c98faeef5fb7c437ef804b3a5924f26
Angikar Roy Chowdhury <angikar.roy.chowdhury@ibm.com>
2021-05-06 17:49:08 +0530

    added category component

---
+++ src/app/category-workout/category-workout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-category-workout',
+  templateUrl: './category-workout.component.html',
+  styleUrls: ['./category-workout.component.css']
+})
+export class CategoryWorkoutComponent implements OnInit {
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
