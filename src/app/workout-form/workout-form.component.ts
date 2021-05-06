6a8222ce2bed0927bef77a543795aa424c934536
Vaibhav Ghadage <vaibhav.ghadage@ibm.com>
2021-05-06 14:51:48 +0530

    added update workout workout service

--- src/app/workout-form/workout-form.component.ts
+++ src/app/workout-form/workout-form.component.ts
@@ -1,4 +1,8 @@
+import { EventEmitter, Input, Output } from '@angular/core';
+import { SimpleChanges } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
+import { Workout } from '../Workout';
+import { WorkoutService } from '../workout.service';
 
 @Component({
   selector: 'app-workout-form',
@@ -6,10 +10,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./workout-form.component.css']
 })
 export class WorkoutFormComponent implements OnInit {
+  @Input() parent: any = { button: "gg" };
+  @Input() workout: Workout = new Workout();
+  @Output() sendWorkouParentt: EventEmitter<Workout> = new EventEmitter<Workout>();
+  categories: any;
 
-  constructor() { }
+  constructor(private workoutService: WorkoutService) { }
+
+  sendWorkout() {
+    this.sendWorkouParentt.emit(this.workout);
+  }
+
+  cancelUpdate() {
+
+  }
+
+  caloriesPlusMinus(increment: number) {
+    this.workout.caloriesBurnt = +this.workout.caloriesBurnt.toPrecision(3) + (+(increment * 0.1).toPrecision(3));
+    this.workout.caloriesBurnt = +this.workout.caloriesBurnt.toPrecision(3);
+    document.getElementById('inputCalories').focus();
+  }
+
 
   ngOnInit(): void {
+    const observable = this.workoutService.getCategories();
+    observable.subscribe(response => {
+      this.categories = response;
+      if (this.categories[0] == undefined) {
+        return alert("No Records available  currently from server");
+      }
+    },
+      error => {
+        if (error.status != 200)
+          return alert("Unable to fetch records from server");
+      });
   }
+
+  ngOnChanges(changes: SimpleChanges) {
+
+    for (let propName in changes) {
+      let change = changes[propName];
+      this.parent = change.currentValue;
+      //this.bug.etaString = this.bug.eta.toString().split('T')[0];
+    }
+  }
+
 
 }
