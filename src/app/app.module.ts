9c0f4bc7e7ac4bd2eb5b30ccdf8475d2b2b4be49
ibmakankshya <akkihiya@gmail.com>
2021-05-06 15:03:16 +0530

    added start and end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StartWorkoutComponent } from './start-workout/start-workout.component';
+import { EndWorkoutComponent } from './end-workout/end-workout.component';
+import {HeaderComponent} from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartWorkoutComponent,
+    EndWorkoutComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/end-workout/end-workout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-end-workout',
+  templateUrl: './end-workout.component.html',
+  styleUrls: ['./end-workout.component.css']
+})
+export class EndWorkoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
